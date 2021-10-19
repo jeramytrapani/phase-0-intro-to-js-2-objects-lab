f6a0a7ba313fad51c5f0b405d86197ddbee0a9fc
jeramytrapani <jeramytrapani@gmail.com>
2021-10-19 16:30:46 -0700

    Completed Again

--- index.js
+++ index.js
@@ -1,23 +1,60 @@
-let employee = {
+const employee = {}
 
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    //return Object.assign({}, employee, { [key]: value})
+    //console.log(employee, key, value);
+    return {...employee, [key]: value}
 }
 
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return Object.assign({}, employee, { [key]: value})
+
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    // console.log(employee, key, value);
+    // employee['name'] = 'George'
+    // console.log(employee)
+    employee[key] = value
+    //console.log(employee)
+    return employee
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    return Object.assign(employee, { [key]: value})
+const deleteFromEmployeeByKey = (employee, key) => {
+    let newObject = Object.assign({}, employee)
+    delete newObject[key];
+    return newObject
 }
 
-function deleteFromEmployeeByKey(employee, key) {
-   const newObject = Object.assign({}, employee)
-   delete newObject[key]
-   return newObject
-}
-
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
     const newObject1 = Object.assign(employee, key)
-   delete newObject1[key]
-   return newObject1
-}
+    delete newObject1[key]
+    return newObject1
+ }
+
+
+
+
+
+
+
+// let employee = {
+
+// }
+
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     return Object.assign({}, employee, { [key]: value})
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//     return Object.assign(employee, { [key]: value})
+// }
+
+// function deleteFromEmployeeByKey(employee, key) {
+//    const newObject = Object.assign({}, employee)
+//    delete newObject[key]
+//    return newObject
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//     const newObject1 = Object.assign(employee, key)
+//    delete newObject1[key]
+//    return newObject1
+// }
